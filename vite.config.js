33717bc57a5a45df5e988e52e9d5fa1cf5932e78
Ariel Martinez <arielmartinez14699@gmail.com>
2025-11-08 18:47:18 -0300

    Agregando elementos a la carpeta assets tales como: .png/.jpg/.avif ademas se agrego un elemento build al archivo vite.cconfig.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,18 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
  base: 'ProyectoConquer', // Reemplaza con el nombre de tu repositorio en GitHub
+ build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          // Si el archivo es CSS, lo llamamos siempre igual
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'assets/style.css';
+          }
+          // Los demás archivos (imágenes, JS, etc.) conservan el formato por defecto
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+  },
 });
